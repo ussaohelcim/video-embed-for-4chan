1ece62db5cf6d71b3633d0b530e85e0683c1a7b3
ussaohelcim <57050328+ussaohelcim@users.noreply.github.com>
2022-05-31 11:53:13 -0300

    upload

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
-# video-embed-for-4chan
+# video-embed-for-4chan  
+
 Chrome extension to embed videos, from a url, into 4chan.
+
+# How to use
+
+Just write `embed://` and link a video url.  
+Example:  
+```
+Hello bros, check out this video:
+embed://https://files.catbox.moe/video.mp4
+```
+
+# How to install
+
+- Download the extension [here](https://github.com/ussaohelcim/video-embed-for-4chan/releases)
+- Go to your browser extensions page
+- Enable developer mode
+- Click on `load unpacked`
+- Select the directory where you saved
+- You are good to go
+
+You can also clone this repo and run `tsc` to build the extension by yourself.
+
+# Manifest
+
+The `manifest.json` its on `/dist/manifest.json`.
+
+# TODO
+
+- create a bookmarklet so you can use on mobile
---
+++ dist/scripts/background.js
@@ -0,0 +1,58 @@
+"use strict";
+chrome.runtime.onInstalled.addListener(() => { });
+chrome.tabs.onUpdated.addListener((id, info, tab) => {
+    if (info.status === 'complete') {
+        chrome.scripting.executeScript({
+            target: { tabId: id },
+            func: init
+        }).then((res) => {
+            console.log(tab.url);
+        }).catch((err) => {
+            throw (err);
+        });
+    }
+});
+function init() {
+    const config = getConfig();
+    const replies = document.querySelectorAll(".postMessage");
+    for (let replyIndex = 0; replyIndex < replies.length; replyIndex++) {
+        const element = replies[replyIndex];
+        let textLines = element.innerHTML.split("<br>");
+        for (let lineIndex = 0; lineIndex < textLines.length; lineIndex++) {
+            if (textLines[lineIndex].startsWith(config.protocol)) {
+                for (const extension of config.extensions) {
+                    if (textLines[lineIndex].endsWith(extension)) {
+                        addVideoToHTMLelement(element, textLines[lineIndex]);
+                    }
+                }
+            }
+        }
+    }
+    function addVideoToHTMLelement(element, embedUrl) {
+        let videoUrl = embedUrl.replace(config.protocol, "");
+        if (videoUrl.includes("<wbr>"))
+            videoUrl = videoUrl.replace("<wbr>", "");
+        console.log(videoUrl);
+        console.log(embedUrl);
+        const videoElement = document.createElement('video');
+        const btn = document.createElement('button');
+        btn.innerText = "Click to embed video.";
+        btn.addEventListener('click', () => {
+            videoElement.controls = true;
+            videoElement.muted = true;
+            videoElement.loop = true;
+            videoElement.autoplay = false;
+            videoElement.src = videoUrl;
+            element.innerHTML += "<br>";
+            element.appendChild(videoElement);
+        });
+        element.innerHTML += "<br>";
+        element.appendChild(btn);
+    }
+    function getConfig() {
+        return {
+            protocol: `embed://`,
+            extensions: [".mp4", ".webm", ".avi"]
+        };
+    }
+}
